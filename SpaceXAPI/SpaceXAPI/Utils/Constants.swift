cd8971a677373ba18d3c9dbb4b4c8b4cc632ef15
Ronald Maciel <ronaldsmaciel2@outlook.com>
2024-03-17 23:25:56 -0300

    add constants and images for readme
    

---
+++ SpaceXAPI/SpaceXAPI/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SpaceXAPI
+//
+//  Created by Ronald on 17/03/24.
+//
+
+import Foundation
+
+struct Constants {
+    struct Service {
+        static let spaceXAPI = "https://api.spacexdata.com/v3/launches"
+    }
+}
